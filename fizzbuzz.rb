76fed9d1dfb7ed085133be088479bc7ec1f86a47
Lluvia Frias <lluvi.frias@gmail.com>
2020-05-04 14:44:41 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 
 def fizzbuzz (i)
-  if i % 3 == 0
-    "Fizz"
+  if i % 3 == 0 && i % 5 == 0
+    "FizzBuzz"
   elsif i % 5 == 0
     "Buzz"
-  else i % 3 == 0 && i % 5 == 0
-    "FizzBuzz"
+  else i % 3 == 0
+  "Fizz"
   end
 
 end
